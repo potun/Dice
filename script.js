ad1bde83b98cb2ca206c56f4bed978bc7d8e0800
potun <62046670+potun@users.noreply.github.com>
2023-01-24 06:06:46 +0000

    Added .die class

--- script.js
+++ script.js
@@ -6,64 +6,44 @@ let images = ["dice-01.svg",
 "dice-06.svg"];
 let dice = document.querySelectorAll("img");
 
-function roll(){
-    dice.forEach(function(die){
+function roll() {
+    let dice = document.querySelectorAll(".die");
+    dice.forEach(function (die) {
         die.classList.add("shake");
     });
-    setTimeout(function(){
-        dice.forEach(function(die){
+    setTimeout(function () {
+        dice.forEach(function (die) {
             die.classList.remove("shake");
         });
-        let dieOneValue = Math.floor(Math.random()*6);
-        let dieTwoValue = Math.floor(Math.random()*6);
-        let dieThreeValue = Math.floor(Math.random()*6);
-        console.log(dieOneValue,dieTwoValue,dieThreeValue);
-        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
-        document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
-        document.querySelector("#die-3").setAttribute("src", images[dieThreeValue]);
-        let result;
-        if(dieOneValue == 3 && dieTwoValue == 4 && dieThreeValue == 5) {
-            result = "You win!";
-        } else if(dieOneValue == 3 && dieTwoValue == 5 && dieThreeValue == 4) {
-            result = "You win!";
-        } else if(dieOneValue == 4 && dieTwoValue == 3 && dieThreeValue == 5) {
-            result = "You win!";
-        } else if(dieOneValue == 4 && dieTwoValue == 5 && dieThreeValue == 3) {
-            result = "You win!";
-        } else if(dieOneValue == 5 && dieTwoValue == 4 && dieThreeValue == 3) {
-            result = "You win!";
-        } else if(dieOneValue == 5 && dieTwoValue == 3 && dieThreeValue == 4) {
-            result = "You win!";
-        } else if(dieOneValue == 0 && dieTwoValue == 1 && dieThreeValue == 2) {
-            result = "You lose";
-        } else if(dieOneValue == 0 && dieTwoValue == 2 && dieThreeValue == 1) {
-            result = "You lose";
-        } else if(dieOneValue == 1 && dieTwoValue == 0 && dieThreeValue == 2) {
-            result = "You lose";
-        } else if(dieOneValue == 1 && dieTwoValue == 2 && dieThreeValue == 0) {
-            result = "You lose";
-        } else if(dieOneValue == 2 && dieTwoValue == 0 && dieThreeValue == 1) {
-            result = "You lose";
-        } else if(dieOneValue == 2 && dieTwoValue == 1 && dieThreeValue == 0) {
-            result = "You lose";
-        } else if(dieOneValue == dieTwoValue && dieOneValue == dieThreeValue) {
-            //House Rule "Ace Out"
-            // if(dieOneValue == 0) {
-            //     result = "Ace out, you lose";
-            // }
-            result = dieOneValue + 1 + " Trips";
-        } else if (dieOneValue == dieTwoValue) {
-            result = (dieThreeValue + 1 + " Points");
-        } else if (dieOneValue == dieThreeValue) {
-            result = (dieTwoValue + 1 + " Points");
-        } else if(dieTwoValue == dieThreeValue) {
-            result = (dieOneValue + 1 + " Points");
+        let dieValues = [];
+        dice.forEach((die) => {
+            dieValues.push(Math.floor(Math.random() * 6));
+            die.setAttribute("src", images[dieValues[dieValues.length - 1]]);
+        });
+        for (let i = 0; i < dieValues.length; i++) {
+            dieValues[i]++;
+        }
+        console.log(dieValues);
+        let result = "";
+        if (dieValues[0] == dieValues[1]) {
+            if (dieValues[1] == dieValues[2]) {
+                result = "Triples! You win!"
+            } else {
+                result += dieValues[2] + " points";
+            }
+        } else if (dieValues[0] == dieValues[2]) {
+            result += dieValues[1] + " points";
+        } else if (dieValues[1] == dieValues[2]) {
+            result += dieValues[0] + " points";
+        } else if (dieValues.includes(4) && dieValues.includes(5) && dieValues.includes(6)) {
+            result = "4-5-6 you WIN!";
+        } else if (dieValues.includes(1) && dieValues.includes(2) && dieValues.includes(3)) {
+            result = "1-2-3 you lose";
         } else {
             result = "Roll again";
         }
+        document.querySelector("#result").innerHTML = result;
+        console.log(result);
 
-        document.querySelector("#total").innerHTML = result;
-    },
-    1000
-    );
-}
+    }, 1000);
+};
