9d24f121cdfe6fcac71a112562296f75f5107c8b
potun <62046670+potun@users.noreply.github.com>
2023-02-02 06:29:14 +0000

    Added ceelo and craps functions

--- js/script.js
+++ js/script.js
@@ -25,28 +25,53 @@ function roll() {
             dieValues[i]++;
         }
         console.log(dieValues);
-        let result = "";
-        if (dieValues[0] == dieValues[1]) {
-            if (dieValues[1] == dieValues[2]) {
-                result = "Triples! You win!"
-            } else {
-                result += dieValues[2] + " points";
-            }
-        } else if (dieValues[0] == dieValues[2]) {
-            result += dieValues[1] + " points";
-        } else if (dieValues[1] == dieValues[2]) {
-            result += dieValues[0] + " points";
-        } else if (dieValues.includes(4) && dieValues.includes(5) && dieValues.includes(6)) {
-            result = "4-5-6 you WIN!";
-        } else if (dieValues.includes(1) && dieValues.includes(2) && dieValues.includes(3)) {
-            result = "1-2-3 you lose";
-        } else {
-            result = "Roll again";
-        }
-        document.querySelector("#result").innerHTML = result;
-        console.log(result);
 
+        document.querySelector("#result").innerHTML = ceelo(dieValues);
     }, 1000);
+}
+
+function ceelo(dieValues) {
+    let result = "";
+    if (dieValues[0] == dieValues[1]) {
+        if (dieValues[1] == dieValues[2]) {
+            result = "Triples! You win!"
+        } else {
+            result += dieValues[2] + " points";
+        }
+    } else if (dieValues[0] == dieValues[2]) {
+        result += dieValues[1] + " points";
+    } else if (dieValues[1] == dieValues[2]) {
+        result += dieValues[0] + " points";
+    } else if (dieValues.includes(4) && dieValues.includes(5) && dieValues.includes(6)) {
+        result = "4-5-6 you WIN!";
+    } else if (dieValues.includes(1) && dieValues.includes(2) && dieValues.includes(3)) {
+        result = "1-2-3 you lose";
+    } else {
+        result = "Roll again";
+    }
+    return result;
+}
+
+function craps(dieValues) {
+    let result = "";
+    if (dieValues[0] == dieValues[1]) {
+        if (dieValues[1] == dieValues[2]) {
+            result = "Triples! You win!"
+        } else {
+            result += dieValues[2] + " points";
+        }
+    } else if (dieValues[0] == dieValues[2]) {
+        result += dieValues[1] + " points";
+    } else if (dieValues[1] == dieValues[2]) {
+        result += dieValues[0] + " points";
+    } else if (dieValues.includes(4) && dieValues.includes(5) && dieValues.includes(6)) {
+        result = "4-5-6 you WIN!";
+    } else if (dieValues.includes(1) && dieValues.includes(2) && dieValues.includes(3)) {
+        result = "1-2-3 you lose";
+    } else {
+        result = "Roll again";
+    }
+    return result;
 }
 
 function addDie() {
